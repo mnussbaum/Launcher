d2cc0e977c220007185cb43daf2aa6d46f523e25
mnussbaum <michaelnussbaum08@gmail.com>
2019-07-26 00:42:44 -0700

    Experiment with query parsing with nom

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate glib;
 extern crate gtk;
+extern crate nom;
 
 use gtk::prelude::*;
 use std;
@@ -10,6 +11,20 @@ mod querier;
 
 use main_window::MainWindow;
 use querier::Querier;
+
+use nom::IResult;
+use nom::bytes::complete::tag;
+use nom::sequence::{separated_pair, terminated};
+use nom::character::complete::alphanumeric1;
+use nom::character::complete::char;
+use nom::combinator::iterator;
+use std::iter::Iterator;
+use std::collections::HashMap;
+use nom::bytes::complete::escaped;
+use nom::character::complete::anychar;
+use nom::character::complete::one_of;
+use nom::sequence::delimited;
+
 
 fn main() {
     gtk::init().expect("Unable to start GTK3. Error");
@@ -24,6 +39,27 @@ fn main() {
             ));
         });
     }
+
+
+    // expirimenting with query parsing
+    let data = "key1:'value1' key2:'value2 woo' key3:value3 ;";
+
+    let mut nom_it = iterator(data, terminated(separated_pair(
+	alphanumeric1,
+	tag(":"),
+	delimited(tag("'"), alphanumeric1, tag("'")), 
+    ), tag(" ")));
+    // replace the alphanumeric1 middle of delimited with something that handles spaces
+    // maybe allow for escaping
+    // handle double quotes
+    // handle non-quoted single words
+
+    let res = nom_it.map(|(k, v)| (k.to_uppercase(), v)).collect::<HashMap<_, _>>();
+
+    let parser_result: IResult<_, _> = nom_it.finish();
+    let (remaining_input, ()) = parser_result.unwrap();
+
+    println!("iterator returned {:?}, remaining input is '{}'", res, remaining_input);
 
     main_window.start();
     gtk::main();
